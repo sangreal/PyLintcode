5763bcc128c98708eeada0a4b5d17ac3315acff3
Martyn Ye <Martyn.Ye@blackboard.com>
2016-07-08 14:22:01 +0800

    update FindKPairswithSmallestSums.py

--- py/FindKPairswithSmallestSums.py
+++ py/FindKPairswithSmallestSums.py
@@ -9,39 +9,35 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        storeMap = collections.defaultdict(list)
         vec = []
         retlist = []
+        dx, dy = [0, 1], [1, 0]
         if len(nums1) == 0 or len(nums2) == 0:
             return retlist
+        storeMap = collections.defaultdict(list)
 
-        i, j = 1, 1
+        len1, len2 = len(nums1), len(nums2)
+
+        visited = [[0 for j in xrange(len2)] for i in xrange(len1)]
+        visited[0][0] = 1
+
+        vec.append(nums1[0]+nums2[0])
+        storeMap[nums1[0]+nums2[0]].append([0, 0])
         heapq.heapify(vec)
-        retlist.append([nums1[0], nums2[0]])
-        k -= 1
-        while i < len(nums1) or j < len(nums2) and k > 0:
-            if i <len(nums1):
-                for k in xrange(j+1):
-                    sum1 = nums1[i]+nums2[k]
-                    heapq.heappush(vec, sum1)
-                    storeMap[sum1].append([nums1[i], nums2[k]])
-            if j < len(nums2):
-                for t in xrange(i):
-                    sum2 = nums1[t] + nums2[j]
-                    heapq.heappush(vec, sum2)
-                    storeMap[sum2].append([nums1[t], nums2[j]])
-            elem = heapq.heappop(vec)
-            retlist.append(storeMap[elem][0])
 
-            storeMap[elem].pop(0)
-            i += 1
-            j += 1
+
+        while len(vec) > 0 and k > 0:
+            cur = heapq.heappop(vec)
+            idx1, idx2 = storeMap[cur][0][0], storeMap[cur][0][1]
+            storeMap[cur].pop(0)
+
+            retlist.append([nums1[idx1], nums2[idx2]])
+            for i in xrange(2):
+                x, y = idx1+dx[i], idx2+dy[i]
+                if x < len1 and y < len2 and visited[x][y] == 0:
+                    heapq.heappush(vec, nums1[x]+nums2[y])
+                    storeMap[nums1[x]+nums2[y]].append([x, y])
+                    visited[x][y] = 1
             k -= 1
 
-        while k > 0:
-            elem = heapq.heappop(vec)
-            retlist.append(storeMap[elem][0])
-
-            storeMap[elem].pop(0)
-            k -= 1
         return retlist
